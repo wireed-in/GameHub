4802d6b6432652eb64cfda4502c84e8927f44cb9
Bala <1428985+wireed-in@users.noreply.github.com>
2023-11-30 21:51:06 -0600

    Refactor: move entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import ApiClient, { FetchResponse } from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new ApiClient<Genre>("/genres");
-
-export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
-}
 
 const useGenres = () =>
     useQuery<FetchResponse<Genre>, Error>({
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import ApiClient, { FetchResponse } from "../services/api-client";
+import { Platform } from "../entities/Platform";
 
 const apiClient = new ApiClient<Platform>("/platforms/lists/parents");
-
-export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-}
 
 const usePlatforms = () =>
     useQuery<FetchResponse<Platform>, Error>({
