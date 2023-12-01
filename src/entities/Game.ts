409e503efc5f8b1b7b12bfeb06a53f6953b37cc9
Bala <1428985+wireed-in@users.noreply.github.com>
2023-12-01 10:09:27 -0600

    Add game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
     id: number;
@@ -8,4 +10,6 @@ export interface Game {
     background_image: string;
     parent_platforms: { platform: Platform }[];
     metacritic: number;
+    genres: Genre[];
+    publishers: Publisher[];
 }
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,8 @@
+import { Box, Divider, Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
-import { Box, Divider, Heading, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
     const slug = useParams().slug;
@@ -18,6 +19,7 @@ const GameDetailPage = () => {
             <Heading>{game.name}</Heading>
             <Divider marginY={2} />
             <ExpandableText>{game.description_raw}</ExpandableText>
+            <GameAttributes game={game} />
         </Box>
     );
 };
